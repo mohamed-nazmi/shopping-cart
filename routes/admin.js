48ccf4cc5da8bb3868fa920da10a155ef2edcb0c
mohamed-nazmi <mohamednazmi96@gmail.com>
2019-08-12 12:19:21 +0200

    Build app's views and handle routes by Express framework

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+// If /products/delete is placed after /products/:productId,
+// it will never be reached because delete will be treated as the product id
+// router.get('/products/delete');
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
