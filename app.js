9c8396e6dfbddc82623511364c82d61c084e7561
mohamed-nazmi <mohamednazmi96@gmail.com>
2019-08-21 20:49:46 +0200

    Encrypt passwords and add login/signup functionalities

--- app.js
+++ app.js
@@ -59,18 +59,6 @@ mongoose
         MONGODB_URI
     )
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Max',
-                    email: 'max@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(3000);
     })
     .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -17,22 +19,55 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('5d59cac9f89a68633c0b7285')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            // To guarantee that the session is saved before
-            // we are redirected to another page
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect('/');
-            });
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            return res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err)
+                });
         })
         .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
-
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            return bcrypt.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    res.redirect('/login');
+                });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
